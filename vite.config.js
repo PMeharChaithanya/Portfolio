a0a42150739c5b0d2947468bde7b1e163a7a9e16
P Mehar Chaithanya <158206032+PMeharChaithanya@users.noreply.github.com>
2024-11-02 12:47:41 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/',
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      'framer-motion',
-      'react-icons/hi',
-      'react-icons/si',
-      'typed.js',
-      'react-type-animation'
-    ]
-  },
+  base: './',
   build: {
     rollupOptions: {
-      external: [],
       output: {
-        manualChunks: {
-          'framer-motion': ['framer-motion'],
-          'react-type-animation': ['react-type-animation'],
-          'typed': ['typed.js']
-        }
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
       }
     }
   }
